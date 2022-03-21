8a0ad857a348c235bcd637344bfe977fba78b01f
unknown <davidpouani228@gmail.com>
2022-03-21 17:29:50 +0100

    projetinitialiser vue

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <HelloWorld msg="Welcome to Your Vue.js App" class="text-3xl font-bold underline"/>
 </template>
 
 <script>
