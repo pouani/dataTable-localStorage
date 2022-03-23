afbc835bef697f31eb68715a9708d67f92058c35
unknown <davidpouani228@gmail.com>
2022-03-23 13:45:56 +0100

    barre de navigation

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,16 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App" class="text-3xl font-bold underline"/>
+  <div id="app">
+      <Navbar />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
+import Navbar from '@/components/Nav.vue';
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Nav,
   }
 }
 </script>
